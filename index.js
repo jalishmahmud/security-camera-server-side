147e808b2ebd20d61f9532afe92f6df00597d956
jalishmahmud <jalish93@gmail.com>
2021-11-13 23:04:07 +0600

    done

--- index.js
+++ index.js
@@ -24,9 +24,17 @@ async function run() {
     const database = client.db("SecurityCamera");
     const usersCollection = database.collection("users");
     const productsCollection = database.collection("products");
+    const ordersCollection = database.collection("orders");
+    const reviewsCollection = database.collection("reviews");
     //find all products
     app.get("/products", async (req, res) => {
       const result = await productsCollection.find({}).toArray();
+      res.json(result);
+    });
+    // add product
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
       res.json(result);
     });
     // find single product
@@ -36,12 +44,73 @@ async function run() {
       const result = await productsCollection.findOne(query);
       res.json(result);
     });
+    // delete product
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // add review
+    app.post("/reviews", async (req, res) => {
+      const product = req.body;
+      const result = await reviewsCollection.insertOne(product);
+      res.json(result);
+    });
+
+    // get review
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find({}).toArray();
+      res.json(result);
+    });
+    // save order
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.json(result);
+    });
+    // find order by user email
+    app.get("/orders/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await ordersCollection.find(query).toArray();
+      res.json(result);
+    });
+    // find all orders
+    app.get("/orders", async (req, res) => {
+      const result = await ordersCollection.find({}).toArray();
+      res.json(result);
+    });
+    // delete order
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+    });
+    // update booking status
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: status.status,
+        },
+      };
+      const result = await ordersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
     // save user to db
     app.post("/users", async (req, res) => {
       const user = req.body;
-      console.log(user);
       const result = await usersCollection.insertOne(user);
-      console.log(result);
       res.json(result);
     });
     // save or update google login user to db
@@ -56,7 +125,6 @@ async function run() {
         options
       );
       res.json(result);
-      console.log(result);
     });
     // save or update uer role as admin
     app.put("/users/admin", async (req, res) => {
@@ -65,7 +133,6 @@ async function run() {
       const updateDoc = { $set: { role: "admin" } };
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.json(result);
-      console.log(result);
     });
     // find user if admin
     app.get("/users/:email", async (req, res) => {
